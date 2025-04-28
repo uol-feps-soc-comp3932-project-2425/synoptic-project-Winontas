98b5cec11f2e24f74528b3077dc58d3f1db97e7d
Winontas <124388092+Winontas@users.noreply.github.com>
2025-04-28 17:00:06 +0100

    Delete tests/frontend directory

--- tests/frontend/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    setupFiles: ['<rootDir>/setup.js'], // Run before module imports
-    testEnvironment: 'jsdom',
-    moduleFileExtensions: ['js'],
-    transform: {
-        '^.+\\.js$': 'babel-jest'
-    }
-};
